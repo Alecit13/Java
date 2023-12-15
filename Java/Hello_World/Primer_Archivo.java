7f0533752dc83a386da6a33fc6c552f67dd3bbdf
Freddy Alejandro Cuadros Contreras <u20221c488@upc.edu.pe>
2023-12-14 22:10:51 -0500

    update

---
+++ Java/Hello_World/Primer_Archivo.java
@@ -0,0 +1,6 @@
+package Java.Hello_World;
+public class Primer_Archivo{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ Java/Variables/Variables.java
@@ -0,0 +1,12 @@
+package Java.Variables;
+
+public class Variables {
+    public static void main(String[] args){
+        int Numero=2;
+        System.out.println("Edad: "+Numero);
+        float flot=2.5f;
+        System.out.println("Flotante: "+flot);
+        double doble = 2.115f;
+        System.out.println("Doble: "+doble);
+    }
+}
